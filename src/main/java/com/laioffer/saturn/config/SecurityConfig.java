f5623d14938e8d3e85868f0ad6b66f3c9d1f8de5
xddcool <102489274+xddcool@users.noreply.github.com>
2022-05-07 00:33:42 -0400

    Revert "add authenticationController, AuthenticationService, JwtUtil, UserNot…"

--- src/main/java/com/laioffer/saturn/config/SecurityConfig.java
+++ src/main/java/com/laioffer/saturn/config/SecurityConfig.java
@@ -1,10 +1,6 @@
 package com.laioffer.saturn.config;
 
 
-
-import org.springframework.security.authentication.AuthenticationManager;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,32 +9,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
-
 
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-    @Autowired
-    private DataSource dataSource;
-
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 
 
 
@@ -47,13 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
 
-                .antMatchers("/search").permitAll()
-                .antMatchers("/authenticate").permitAll()
-
-
                 .antMatchers(HttpMethod.POST, "/register").permitAll()
                 .antMatchers("/search").permitAll()
-
                 .anyRequest().authenticated()
                 .and()
                 .csrf()
@@ -61,33 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .passwordEncoder(passwordEncoder())
-                .usersByUsernameQuery("SELECT username, password, enabled FROM user WHERE username = ?")
-                .authoritiesByUsernameQuery("SELECT username, authority FROM authority WHERE username = ?");
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 }
-
-
-    }
-
-}
-
 
 
 
